ce30ed8db5b7258b8d119c085cce0f6f302a1cfb
Shashi <sharmashashi940@gmail.com>
2020-12-10 19:04:33 +0545

    introduction to basic widgets

---
+++ android/app/src/main/kotlin/com/example/fluttergca/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttergca
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fluttergca/screens/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
