70e626cdff1a10fac101983a0ff2b0631e03b1e6
Shashi <sharmashashi940@gmail.com>
2020-12-11 19:02:26 +0545

    stateful and stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:fluttergca/screens/home.dart';
+import 'package:fluttergca/screens/mycounter.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomePage(),
+    home: MyCounter(),
   ));
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,75 +1,76 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String changeValue = "Initial";
+  String gesture = "no gesture provided yet";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          actions: [
-            Icon(
-              Icons.phone,
-              color: Colors.white,
-            ),
-            SizedBox(
-              width: 30,
-            ),
-            Icon(
-              Icons.video_call,
-              color: Colors.white,
-            ),
-            SizedBox(
-              width: 20,
-            )
-          ],
-          title: Text("This is title"),
-        ),
+        appBar: AppBar(),
         body: Column(
           children: [
-            Container(height: 50, width: 100, color: Colors.purple),
-            SizedBox(
-              height: 20,
+            GestureDetector(
+              onTap: () {
+                handleGesture("on tap called");
+              },
+              onLongPress: () {
+                handleGesture("on long press called");
+              },
+              onDoubleTap: () {
+                handleGesture("on double tap called");
+              },
+              child: Text("Provide gesture here"),
             ),
-            Container(height: 50, width: 100, color: Colors.green),
-            SizedBox(
-              height: 20,
-            ),
-            Container(height: 50, width: 100, color: Colors.purple),
-            Text(
-              "This is text",
-              style: TextStyle(color: Colors.red),
-            ),
-            TextField(),
             RaisedButton(
-              elevation: 5,
-              color: Colors.amber,
-              onPressed: () {},
-              child: Text("Raised button"),
+              onPressed: () {
+                changeValue = "Changed";
+
+                print(changeValue);
+                setState(() {});
+              },
+              child: Text("Click Me"),
             ),
-            FlatButton(
-              onPressed: () {},
-              child: Text("Flat Button"),
-              color: Colors.red,
-            ),
-            Image.network(
-              "https://course.genesecloud.academy/pluginfile.php?file=%2F1%2Ftheme_alpha%2Fcustomlogotopbar%2F1606229507%2Fsmall-icon.png",
-              height: 50,
-              width: 50,
-            ),
-            Container(
-              color: Colors.transparent,
-              height: 200,
-              width: 200,
-              child: Stack(
-                children: [
-                  Container(height: 200, width: 200, color: Colors.purple),
-                  Container(height: 150, width: 150, color: Colors.green),
-                  Text("I am at top"),
-                ],
-              ),
-            ),
-            Container(
-                height: 50, width: 50, color: Color.fromRGBO(255, 0, 0, 0.5))
+            Row(
+              children: [
+                Text(
+                  "Gesture Statue: ",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(gesture)
+              ],
+            )
+          ],
+        ));
+  }
+
+  void handleGesture(String providedGesture) {
+    setState(() {
+      gesture = providedGesture;
+    });
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  String changeValue = "Initial";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Text(changeValue),
+            RaisedButton(
+              onPressed: () {
+                changeValue = "Changed";
+                print(changeValue);
+              },
+              child: Text("Click Me"),
+            )
           ],
         ));
   }
